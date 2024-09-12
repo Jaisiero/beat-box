1db050dfc285f1b7d6ede81807a629edf4ca010d
Jaime Sierra <jaisiero@gmail.com>
2024-09-12 14:07:36 -0400

    Window added

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,16 @@
-#include <iostream>
+#include "window.hpp"
 
 int main(int argc, char const *argv[])
 {
-    std::cout << "Hello World!" << std::endl;
+    // Create a window
+    auto window = AppWindow("Beat Box", 860, 640);
+
+    // Daxa code goes here...
+
+    while (!window.should_close())
+    {
+        window.update();
+    }
+
     return 0;
 }
